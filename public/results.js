7aea2aaf6c62b2026af33fbc7debe43aef590440
recu3125 <superhoy0509@gmail.com>
2023-07-03 23:07:45 +0900

    feat ask to suggest a new word when all answered

--- public/results.js
+++ public/results.js
@@ -1,6 +1,6 @@
 const localStorage = window.localStorage;
 const container = document.getElementById('container');
-
+let allAnswered = true
 fetch('/resources/wordData.json')
   .then(async (wordData) => {
     wordData = await wordData.json();
@@ -26,5 +26,14 @@ fetch('/resources/wordData.json')
 
         container.appendChild(wordbtn);
       }
+      else {
+        allAnswered = false
+      }
+    }
+    if (allAnswered) {
+      const allAnsParagraph = document.createElement('p')
+      allAnsParagraph.className = 'title'
+      allAnsParagraph.innerHTML = "Congratulations! You've provided answers for all the words on my website!<br>Now you can click <a href='https://forms.gle/Wg7Y2Wq6UdWueqRF8' target='_blank'>here</a> to suggest a new word to be added in Colorword!<br>Thank you for your contribution!"
+      document.getElementById('top').appendChild(allAnsParagraph)
     }
   });
