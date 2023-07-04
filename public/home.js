bd250faf33ab9db4a6a7073f02cddd85c763bde4
recu3125 <superhoy0509@gmail.com>
2023-07-04 14:04:59 +0900

    add scrolldown button in home

--- public/home.js
+++ public/home.js
@@ -1,21 +1,55 @@
-const scrollingText = document.getElementById('scrollingText');
-const lines = scrollingText.getElementsByTagName('br').length + 1;
+fetch('/resources/wordData.json')
+  .then(async (wordData) => {
+    const scrollingText = document.getElementById('scrollingText');
+    wordData = await wordData.json();
+    words = wordData.words
+    text = ''
+    for (i = 0; i < words.length; i++) {
+      text += words[i] + '<br>'
+    }
+    text += words[0]
+    console.log(text)
+    scrollingText.innerHTML = text
+    const lines = scrollingText.getElementsByTagName('br').length + 1;
 
-let keyframes = '';
-let pausePercentage = 0;
+    let keyframes = '';
+    let pausePercentage = 0;
 
-for (let i = 0; i < lines - 1; i++) {
-  keyframes += `${pausePercentage}% { transform: translateY(-${i * 100 * (lines - 1) / (lines - 1) / lines}%); } `;
-  pausePercentage += 100 / (lines - 1);
-}
+    for (let i = 0; i < lines - 1; i++) {
+      keyframes += `${pausePercentage}% { transform: translateY(-${i * 100 * (lines - 1) / (lines - 1) / lines}%); } `;
+      pausePercentage += 100 / (lines - 1);
+    }
 
-const styleSheet = document.createElement('style');
-styleSheet.innerHTML = `
-  @keyframes scroll {
-    ${keyframes}
-    100% { transform: translateY(-${100 * (lines - 1) / (lines)}%); ); }
+    const styleSheet = document.createElement('style');
+    styleSheet.innerHTML = `@keyframes scroll {${keyframes}100% { transform: translateY(-${100 * (lines - 1) / (lines)}%); ); }}`;
+
+    document.head.appendChild(styleSheet);
+  })
+function scrollinfo() {
+  const infoSection = document.getElementById('info-section');
+  const infoSectionHeight = infoSection.clientHeight;
+  const startPosition = window.scrollY;
+  const targetPosition = infoSection.offsetTop - (window.innerHeight - infoSectionHeight);
+  const distance = targetPosition - startPosition;
+  const duration = 1000; // Adjust this value to control the scroll speed (in milliseconds)
+  let startTime = null;
+
+  function scrollStep(timestamp) {
+    if (!startTime) startTime = timestamp;
+    const progress = timestamp - startTime;
+    const easeProgress = Math.min(progress / duration, 1);
+    const easing = easeInOutQuad(easeProgress);
+    window.scrollTo(0, startPosition + distance * easing);
+
+    if (progress < duration) {
+      requestAnimationFrame(scrollStep);
+    }
   }
-`;
 
-document.head.appendChild(styleSheet);
+  // Easing function
+  function easeInOutQuad(t) {
+    return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
+  }
 
+  requestAnimationFrame(scrollStep);
+}
