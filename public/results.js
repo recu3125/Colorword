e777d028a40301061b9bf9c85fe4078804e3f31c
recu3125 <superhoy0509@gmail.com>
2023-07-04 09:41:31 +0900

    feat confetti when allAnswered

--- public/results.js
+++ public/results.js
@@ -35,5 +35,33 @@ fetch('/resources/wordData.json')
       allAnsParagraph.className = 'title'
       allAnsParagraph.innerHTML = "Congratulations! You've provided answers for all the words on my website!<br>Now you can click <a href='https://forms.gle/Wg7Y2Wq6UdWueqRF8' target='_blank'>here</a> to suggest a new word to be added in Colorword!<br>Thank you for your contribution!"
       document.getElementById('top').appendChild(allAnsParagraph)
+      flyconfetti()
     }
   });
+
+function flyconfetti() {
+  var end = Date.now() + (7 * 1000);
+  var colors = ["#6127f8", "#24e071", "#84a1b2", "#95cffe", "#9e1ada", "#d78b76", "#aed26e", "#3917f7", "#90c35d", "#42da06", "#694fc6", "#d3b272", "#fee22e", "#12ba9b", "#1bf1b4", "#6af91b", "#45004b", "#78db95", "#b9db2c", "#5b1621", "#d9992e", "#e2fb34", "#adaf9b", "#537f44", "#ef7278", "#230ee2", "#26cbcf",];
+
+  (function frame() {
+    var color = [colors[Math.floor(Math.random() * colors.length)], colors[Math.floor(Math.random() * colors.length)]]
+    confetti({
+      particleCount: 2,
+      angle: 60,
+      spread: 55,
+      origin: { x: 0 },
+      colors: color
+    });
+    confetti({
+      particleCount: 2,
+      angle: 120,
+      spread: 55,
+      origin: { x: 1 },
+      colors: color
+    });
+
+    if (Date.now() < end) {
+      requestAnimationFrame(frame);
+    }
+  }());
+}
